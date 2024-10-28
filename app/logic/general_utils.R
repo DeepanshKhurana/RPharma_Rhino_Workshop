32b1b6b9af4bc7a79af357685a3652c63e745b61
deepanshkhurana <deepanshkhurana@outlook.com>
2024-10-28 17:26:29 +0530

    chore: function structure improvement for is_convertible_to_date

--- app/logic/general_utils.R
+++ app/logic/general_utils.R
@@ -1,8 +1,14 @@
 #' Check if a column is a datetime column
 #' @param column A column from a data frame
+#' @param dataset A data frame
+#' @param format The format of the date column
 #' @return TRUE if the column is convertible to date, FALSE otherwise
 #' @export
-is_convertible_to_date <- function(column, dataset, format = "%Y-%m-%d") {
+is_convertible_to_date <- function(
+  column,
+  dataset,
+  format = "%Y-%m-%d"
+) {
   tryCatch({
     converted <- as.Date(dataset[[column]], format)
     any(!is.na(converted))
